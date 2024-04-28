801bae380da91b8444a55c3f58d4ae5da0bc75b2
t.uehara <t.uehara@gmail.com>
2024-04-28 21:24:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import elegantt
+
 chartsize = (720,320)
 bgcolor = (255,255,255)
+
 gchart = elegantt.EleGantt( chartsize, bgcolor, today="2019-10-15")
+gchart.set_font("ipaexg.ttf")
 gchart.draw_calendar()
-gchart.draw_campain("2019-10-15","2019-10-18","Hello")
-gchart.draw_campain("2019-10-20","2019-10-23","Hello")
-gchart.draw_campain("2019-10-24","2019-10-29","Hello")
+gchart.draw_campain("2019-10-15","2019-10-18","こんにちは")
+gchart.draw_campain("2019-10-20","2019-10-23","こんにちは")
+gchart.draw_campain("2019-10-24","2019-10-29","こんにちは")
 gchart.save("test.png")
